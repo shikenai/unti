fad8ea954a65cb9f30ba777587aacb34707c0194
shikenai <kakehashi.y2788@gmail.com>
2023-02-03 12:04:05 +0900

    極めてシンプルなコンポーネント導入

---
+++ front/src/components/MyTest.vue
@@ -0,0 +1,14 @@
+<template>
+  <h1>testあああ</h1>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+// export default {
+//   name: "MyTest"
+// }
+</script>
+
+<style scoped>
+
+</style>
--- myapp/views.py
+++ myapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-
+from myapp.management import commands
 # Create your views here.
 
 
