41714283e15f5f0fe9afccb6d019231ecab6d04c
shikenai <kakehashi.y2788@gmail.com>
2023-02-03 23:01:31 +0900

    日数とコードを指定すると、その銘柄の推移を見ることができるようになった

--- myapp/management/commands/drawer.py
+++ myapp/management/commands/drawer.py
@@ -3,20 +3,13 @@ from django.core.management.base import BaseCommand
 import matplotlib
 import matplotlib.pyplot as plt
 from django.http import HttpResponse
+from myapp.models import Trades
+import pandas as pd
+import mplfinance as mf
+from django_pandas.io import read_frame
 
 # バックエンドを指定
 matplotlib.use('Agg')
-
-
-# グラフ作成
-def set_plt():
-    print('!')
-    x = ["07/01", "07/02", "07/03", "07/04", "07/05", "07/06", "07/07"]
-    y = [3, 5, 0, 5, 6, 10, 2]
-    plt.bar(x, y, color='#00d5ff')
-    plt.title(r"$\bf{Running Trend  -2020/07/07}$", color='#3407ba')
-    # plt.xlabel("Date")
-    # plt.ylabel("km")
 
 
 # SVG化
@@ -29,8 +22,25 @@ def plt2svg():
 
 
 # 実行するビュー関数
-def get_svg():
-    set_plt()
+def get_svg2http_response(days, brand_code):
+    _trades = Trades.objects.filter(brand_code=brand_code).order_by("trade_date")
+    n = _trades.count()
+    x = days
+    if n < x:
+        n_minus = 0
+    else:
+        n_minus = n - x
+    df = read_frame(_trades.all()[n_minus:n])
+
+    df["trade_date"] = pd.to_datetime(df["trade_date"])
+    df = df.set_index("trade_date")
+    df = df.reindex(columns=['open_value', 'high_value', 'low_value', 'close_value', 'volume'])
+    df = df.rename(columns={'open_value': "Open", 'high_value': "High", 'low_value': "Low", 'close_value': "Close",
+                            'volume': "Volume"})
+    mf.plot(df,
+            type="candle",
+            mav=[5, 12],
+            volume=True)
     svg = plt2svg()  # SVG化
     plt.cla()  # グラフをリセット
     response = HttpResponse(svg, content_type='image/svg+xml')
@@ -46,4 +56,4 @@ class Command(BaseCommand):
         parser.add_argument("first", type=str)
 
     def handle(self, *args, **options):
-        get_svg()
+        get_svg2http_response()
--- myapp/views.py
+++ myapp/views.py
@@ -8,32 +8,8 @@ import pandas as pd
 
 
 def index(request):
-    if True:
-        _trades = Trades.objects.filter(brand_code="1301.jp").order_by("trade_date")
-        for t in _trades.all()[0:15]:
-            print(t.values())
-        df = read_frame(_trades.all()[0:15])
-        df["trade_date"] = pd.to_datetime(df["trade_date"])
-        df = df.set_index("trade_date")
-        df = df.reindex(columns=['open_value', 'high_value', 'low_value', 'close_value', 'volume'])
-        df = df.rename(columns={'open_value': "Open", 'high_value': "High", 'low_value': "Low", 'close_value': "Close",
-                                'volume': "Volume"})
-        mf.plot(df, type="line")
-
-    print(type(df))
-
-    # stocks.reg_brands_from_csv()　実施済み
-    # stocks.reg_trades_from_csv() 実施済み 20分くらいかかったので、改善希望
-    # return JsonResponse({"user": [
-    #     {"id": 1, "name": "taro"},
-    #     {"id": 2, "name": "jiro"},
-    # ]})
-
-    plt = drawer.get_svg()
-    print(type(plt))
-    print(plt)
+    plt = drawer.get_svg2http_response(250, "7203.jp")
     return HttpResponse(plt)
-    # return HttpResponse("hello")
 
 
 def home(request):
