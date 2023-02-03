9533553bbe81d24db05634d7559b97eaedd806cb
shikenai <kakehashi.y2788@gmail.com>
2023-02-03 12:57:14 +0900

    axios.getをテンプレートに流し込むことができた気がする

--- front/src/components/MyTest.vue
+++ front/src/components/MyTest.vue
@@ -1,19 +1,27 @@
-<template>
-  <h1>testあああ</h1>
-</template>
-
 <script setup lang="ts">
 import axios from "axios";
+import {ref} from "vue";
 
-window.onload = () => {
-  console.log("test console")
-  axios.get("api/index.json").then(res => {
-    console.log(res)
-    console.log(res.data.user)
-    const testname = res.data.user
-  })
-}
+const mytext = "helloworld"
+const wei = ref('untitti')
+
+console.log("test console")
+let geta = ref()
+axios.get("api/index.json").then(res => {
+  console.log(res)
+  console.log(res.data.user)
+  geta.value = ref(res.data.user)
+})
 </script>
+
+
+<template>
+  <h1>testあああ</h1>
+  <h6>{{ mytext }}</h6>
+  <h4>{{ wei }}</h4>
+  <h4>{{ geta }}</h4>
+</template>
+
 
 <style scoped>
 
