a33f10d9b36b3f0a4575b06c186246ce47abdb87
shikenai <kakehashi.y2788@gmail.com>
2023-02-03 23:14:49 +0900

    銘柄推移データがあるときだけ、svg表示スペースを確保するようにした。

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
 import MyTest from "./components/MyTest.vue";
 import axios from "axios";
 import {ref} from "vue";
@@ -16,28 +15,16 @@ const clicked = () => {
 <template>
   <nav>
     <ul>
-      <li><a href="http://localhost:8000/admin/">管理サイト</a></li>
+      <li><a href="">home</a></li>
       <li><a href="/api/index.json" @click.prevent="clicked">index.json</a></li>
+      <li><a href="http://localhost:8000/admin/">管理サイト</a></li>
     </ul>
   </nav>
-  <div v-html="txt" style="width: 640px; height: 480px;"></div>
+  <div v-if="txt !=='dummy2'" v-html="txt" style="width: 640px; height: 480px;"></div>
   <MyTest/>
-  <HelloWorld msg="unti vue"/>
+  <!--  <HelloWorld msg="unti vue"/>-->
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
