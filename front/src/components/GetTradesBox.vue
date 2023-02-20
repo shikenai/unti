913f8db90c4b270c63cad3a6d64483c255e91e4d
shikenai <kakehashi.y2788@gmail.com>
2023-02-20 18:36:51 +0900

    取引情報の取得をするためのコンポーネントを作ろうと思った その２

--- front/src/components/GetTradesBox.vue
+++ front/src/components/GetTradesBox.vue
@@ -1,11 +1,33 @@
-
 <script setup lang="ts">
-
+const unti = () => {
+  console.log('untitititi')
+}
 </script>
 <template>
-  <h2>gettrade</h2>
+  <div class="main">
+    <div class="left_bar">
+    <a href="" class="btn" @click="unti">Button</a>
+    </div>
+    <div class="drawing">
+    </div>
+  </div>
 </template>
 
 <style scoped>
 
+.main {
+  display: flex;
+}
+
+.left_bar {
+  width: 150px;
+  padding-top: 5px;
+  border: solid black 1px;
+  min-height: 800px;
+}
+
+.drawing {
+  border: solid black 1px;
+  min-height: 800px;
+}
 </style>
--- front/src/components/SearchBox.vue
+++ front/src/components/SearchBox.vue
@@ -21,28 +21,28 @@ const onKeyPressEnter = () => {
     txt.value = res.data
   })
 }
-let postData = ref({brand_code:"", days:0})
+let postData = ref({brand_code: "", days: 0})
 
 </script>
 <template>
   <div class="main">
-<!--  <button @click="get_brands">botan</button>-->
-  <div class="left_bar">
-    <label for="brand_code">銘 柄 名：</label>
-    <input id="brand_code" type="text" v-model="postData.brand_code" @keypress.enter="onKeyPressEnter" ><br>
-    <label for="days">取得日数：</label>
-    <input id="days" type="text" v-model="postData.days" @keypress.enter="onKeyPressEnter">
-<!--    <table>-->
-<!--      <tr v-for="c in companies.brands" :key="c.code">-->
-<!--        <td v-text="c.code"></td>-->
-<!--        <td v-text="c.brand_name"></td>-->
-<!--      </tr>-->
-<!--    </table>-->
-  </div>
-  <div class="drawing">
-    <div id="main" v-if="txt !=='dummy2'" v-html="txt" style="width: 640px; height: 480px;"></div>
-  </div>
+    <!--  <button @click="get_brands">botan</button>-->
+    <div class="left_bar">
+      <label for="brand_code">銘 柄 名：</label>
+      <input id="brand_code" type="text" v-model="postData.brand_code" @keypress.enter="onKeyPressEnter"><br>
+      <label for="days">取得日数：</label>
+      <input id="days" type="text" v-model="postData.days" @keypress.enter="onKeyPressEnter">
+      <!--    <table>-->
+      <!--      <tr v-for="c in companies.brands" :key="c.code">-->
+      <!--        <td v-text="c.code"></td>-->
+      <!--        <td v-text="c.brand_name"></td>-->
+      <!--      </tr>-->
+      <!--    </table>-->
     </div>
+    <div class="drawing">
+      <div id="main" v-if="txt !=='dummy2'" v-html="txt" style="width: 640px; height: 480px;"></div>
+    </div>
+  </div>
 </template>
 
 <style scoped>
@@ -50,6 +50,7 @@ let postData = ref({brand_code:"", days:0})
 .main {
   display: flex;
 }
+
 .left_bar {
   width: 150px;
   padding-top: 5px;
