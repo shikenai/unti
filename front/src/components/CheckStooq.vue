a1b7e947cf227c47f13a17f8bc32c2b0e47f61de
shikenai <kakehashi.y2788@gmail.com>
2023-03-05 17:25:25 +0900

    stooq api が使えなくなったかどうかのチェック機能を追加

---
+++ front/src/components/CheckStooq.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import axios from "axios";
+import {ref} from "vue";
+
+const mytext = "helloworld"
+const wei = ref('untitti')
+
+console.log("test console")
+let geta = ref([])
+
+const check_stooq = () => {
+  axios.get('api/check_stooq').then(res => {
+  })
+}
+</script>
+
+
+<template>
+  <a href="" @click.prevent="check_stooq">check stooq</a>
+</template>
+
+
+<style scoped>
+
+</style>
